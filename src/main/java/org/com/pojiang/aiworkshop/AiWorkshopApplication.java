902c7e7de918ae349be925bfb026023c44fc25b5
pojiang <2114654816@qq.com>
2025-08-30 21:14:55 +0800

    利用智浦轻言启动项目

--- src/main/java/org/com/pojiang/aiworkshop/AiWorkshopApplication.java
+++ src/main/java/org/com/pojiang/aiworkshop/AiWorkshopApplication.java
@@ -3,6 +3,9 @@ package org.com.pojiang.aiworkshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author liheng
+ */
 @SpringBootApplication
 public class AiWorkshopApplication {
 
---
+++ src/main/java/org/com/pojiang/aiworkshop/controller/ChatController.java
@@ -0,0 +1,37 @@
+package org.com.pojiang.aiworkshop.controller;
+
+import java.util.Map;
+
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.zhipuai.ZhiPuAiChatModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Flux;
+
+@RestController
+public class ChatController {
+
+    private final ZhiPuAiChatModel chatModel;
+
+    @Autowired
+    public ChatController(ZhiPuAiChatModel chatModel) {
+        this.chatModel = chatModel;
+    }
+
+    @GetMapping("/ai/generate")
+    public Map generate(@RequestParam(value = "message", defaultValue = "讲一个笑话") String message) {
+        return Map.of("generation", this.chatModel.call(message));
+    }
+
+    @GetMapping("/ai/generateStream")
+    public Flux<ChatResponse> generateStream(
+            @RequestParam(value = "message", defaultValue = "讲一个笑话") String message) {
+        var prompt = new Prompt(new UserMessage(message));
+        return this.chatModel.stream(prompt);
+    }
+}
