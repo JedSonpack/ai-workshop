7a23ae6f4762548229d8327f90e71759dc50a73e
pojiang <2114654816@qq.com>
2025-08-30 16:53:01 +0800

    初始化

---
+++ src/main/java/org/com/pojiang/aiworkshop/AiWorkshopApplication.java
@@ -0,0 +1,13 @@
+package org.com.pojiang.aiworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiWorkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiWorkshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/com/pojiang/aiworkshop/AiWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.com.pojiang.aiworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiWorkshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
